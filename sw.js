5e91c9720d81217029aad2121f6196e8e13dc841
nattawutsuwansopa-afk <nattawutsuwansopa@gmail.com>
2026-09-09 15:08:24 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE='rattana-pos-pwa-v5-1-money-commas';
+const CORE=['./','./index.html','./manifest.webmanifest','./offline.html','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  const req=event.request;if(req.method!=='GET')return;
+  if(req.mode==='navigate'){
+    event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html').then(r=>r||caches.match('./offline.html'))));return;
+  }
+  const url=new URL(req.url);
+  if(url.origin===self.location.origin){
+    event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{if(res&&res.status===200){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy))}return res})));return;
+  }
+  event.respondWith(caches.match(req).then(cached=>{
+    const fresh=fetch(req).then(res=>{if(res&&(res.ok||res.type==='opaque')){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy))}return res}).catch(()=>cached);
+    return cached||fresh;
+  }));
+});
