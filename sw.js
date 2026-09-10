9a042310f2fb816f0a6f842512f1fd8df1b00107
nattawutsuwansopa-afk <nattawutsuwansopa@gmail.com>
2026-09-10 11:44:35 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rattana-pos-pwa-v6-6-custom-sale-settings';
+const CACHE='rattana-pos-v6.11';
 const CORE=['./','./index.html','./manifest.webmanifest','./offline.html','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
